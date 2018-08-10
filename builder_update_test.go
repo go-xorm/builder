df941ac9ef63468508b3ea92e739ffa6de0e4f81
Lunny Xiao <xiaolunwen@gmail.com>
2018-08-10 10:39:17 +0800

    More update tests (#25)
    * add more tests for update
    
    * more tests for update

--- builder_update_test.go
+++ builder_update_test.go
@@ -21,6 +21,11 @@ func TestBuilderUpdate(t *testing.T) {
 	assert.EqualValues(t, "UPDATE table1 SET a=?,b=? WHERE a=?", sql)
 	assert.EqualValues(t, []interface{}{2, 1, 1}, args)
 
+	sql, args, err = Update(Eq{"a": 2}, Eq{"b": 1}).From("table1").Where(Eq{"a": 1}).ToSQL()
+	assert.NoError(t, err)
+	assert.EqualValues(t, "UPDATE table1 SET a=?,b=? WHERE a=?", sql)
+	assert.EqualValues(t, []interface{}{2, 1, 1}, args)
+
 	sql, args, err = Update(Eq{"a": 2, "b": Incr(1)}).From("table2").Where(Eq{"a": 1}).ToSQL()
 	assert.NoError(t, err)
 	assert.EqualValues(t, "UPDATE table2 SET a=?,b=b+? WHERE a=?", sql)
@@ -38,4 +43,10 @@ func TestBuilderUpdate(t *testing.T) {
 	sql, args, err = Update(Eq{}).From("table1").Where(Eq{"a": 1}).ToSQL()
 	assert.Error(t, err)
 	assert.EqualValues(t, ErrNoColumnToUpdate, err)
+
+	var builder = Builder{cond: NewCond()}
+	sql, args, err = builder.Update(Eq{"a": 2, "b": 1}).From("table1").Where(Eq{"a": 1}).ToSQL()
+	assert.NoError(t, err)
+	assert.EqualValues(t, "UPDATE table1 SET a=?,b=? WHERE a=?", sql)
+	assert.EqualValues(t, []interface{}{2, 1, 1}, args)
 }
