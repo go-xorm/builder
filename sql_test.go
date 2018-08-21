1a34f461860e4320777b1b9142a100a1a0e8b10d
zhanghelong <outman99@hotmail.com>
2018-08-21 17:02:06 +0800

    merge sql_fidder -> sql_test

--- sql_test.go
+++ sql_test.go
@@ -5,8 +5,12 @@
 package builder
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 
+	"github.com/go-xorm/sqlfiddle"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -58,4 +62,100 @@ func TestSQL(t *testing.T) {
 	newSQL, args, err = ToSQL(1)
 	assert.Error(t, err)
 	assert.EqualValues(t, ErrNotSupportType, err)
+}
+
+type fiddler struct {
+	sessionCode string
+	dbType      int
+	f           *sqlfiddle.Fiddle
+}
+
+func readPreparationSQLFromFile(path string) (string, error) {
+	file, err := os.Open(path)
+	defer file.Close()
+	if err != nil {
+		return "", err
+	}
+
+	data, err := ioutil.ReadAll(file)
+	if err != nil {
+		return "", err
+	}
+
+	return string(data), nil
+}
+
+func newFiddler(fiddleServerAddr, dbDialect, preparationSQL string) (*fiddler, error) {
+	var dbType int
+	switch dbDialect {
+	case MYSQL:
+		dbType = sqlfiddle.Mysql5_6
+	case MSSQL:
+		dbType = sqlfiddle.MSSQL2017
+	case POSTGRES:
+		dbType = sqlfiddle.PostgreSQL96
+	case ORACLE:
+		dbType = sqlfiddle.Oracle11gR2
+	case SQLITE:
+		dbType = sqlfiddle.SQLite_WebSQL
+	default:
+		return nil, ErrNotSupportDialectType
+	}
+
+	f := sqlfiddle.NewFiddle(fiddleServerAddr)
+	response, err := f.CreateSchema(dbType, preparationSQL)
+	if err != nil {
+		return nil, err
+	}
+
+	return &fiddler{sessionCode: response.Code, f: f, dbType: dbType}, nil
+}
+
+func (f *fiddler) executableCheck(obj interface{}) error {
+	var sql string
+	var err error
+	switch obj.(type) {
+	case *Builder:
+		sql, err = obj.(*Builder).ToBindedSQL()
+		if err != nil {
+			return err
+		}
+	case string:
+		sql = obj.(string)
+	}
+
+	_, err = f.f.RunSQL(f.dbType, f.sessionCode, sql)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func TestReadPreparationSQLFromFile(t *testing.T) {
+	sqlFromFile, err := readPreparationSQLFromFile("testdata/mysql_fiddle_data.sql")
+	assert.NoError(t, err)
+	fmt.Println(sqlFromFile)
+}
+
+func TestNewFiddler(t *testing.T) {
+	sqlFromFile, err := readPreparationSQLFromFile("testdata/mysql_fiddle_data.sql")
+	assert.NoError(t, err)
+	f, err := newFiddler("", MYSQL, sqlFromFile)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, f.sessionCode)
+}
+
+func TestExecutableCheck(t *testing.T) {
+	sqlFromFile, err := readPreparationSQLFromFile("testdata/mysql_fiddle_data.sql")
+	assert.NoError(t, err)
+	f, err := newFiddler("", MYSQL, sqlFromFile)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, f.sessionCode)
+
+	assert.NoError(t, f.executableCheck("SELECT * FROM table1"))
+
+	err = f.executableCheck("SELECT * FROM table3")
+	assert.Error(t, err)
+	fmt.Println(err)
 }
